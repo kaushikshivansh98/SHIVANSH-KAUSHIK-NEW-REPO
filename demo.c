f1b8aacc0049e05d49f74bc2161c0e60b6b9f9fb
SHIVANSH KAUSHIK <kaushikshivansh98@gmail.com>
2025-09-29 23:10:21 +0530

    First clean commit without big files

---
+++ demo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("shivansh kaushik");
+
+    return 0;
+
+}
---
+++ greatest.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	int b; 
+	int c; //declaration variables
+	cin >>a>>b>>c; //user input
+	if (a>=b && a>=c)
+	{
+	    cout<<"a is largest"<<a;
+	    
+	}
+	else if (b>=a && b>=c)
+	{
+	    cout<<"b is largest"<<b;
+	    
+	}
+	else 
+	{
+	    cout<<"c is largest"<<c;
+	}
+	
+	return 0;
+	
+	
+	
+
+}
