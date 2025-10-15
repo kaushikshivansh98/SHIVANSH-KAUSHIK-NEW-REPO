731886365be91b89e91d56b7af62e30ae3d9c303
SHIVANSH KAUSHIK <kaushikshivansh98@gmail.com>
2025-10-15 12:51:09 +0530

    exp 2

--- demo.cpp
+++ demo.cpp
@@ -3,5 +3,13 @@ using namespace std;
 int main()
 {
     cout <<"hello world";
+    int a;
+    float b;
+    float d;
+    string c;
+    cout<<"enter no";
+    cin>>a>>b>>d>>c;
+    cout<<b/d;
+
     return 0;
 }
